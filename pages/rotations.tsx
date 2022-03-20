f7aa88843ad52fed46ff35cf3a8ca754453e134f
Matt <mqunell@gmail.com>
2022-03-19 20:19:18 -0700

    make rotations table more responsive

--- pages/rotations.tsx
+++ pages/rotations.tsx
@@ -1,8 +1,8 @@
+import { Fragment } from 'react';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import classNames from 'classnames';
 import { Trip, Block, getTrips } from '../lib/trips';
-import { Fragment } from 'react';
 
 export const getServerSideProps: GetServerSideProps = async () => {
 	const trips = await getTrips();
@@ -23,7 +23,7 @@ export default function Rotations({ trips }) {
 	});
 
 	return (
-		<div className="flex flex-col items-center gap-2 p-4">
+		<div className="flex h-screen flex-col items-center gap-2 p-4">
 			<Head>
 				<title>Flight Log</title>
 				<meta name="description" content="Next.js App" />
@@ -32,57 +32,62 @@ export default function Rotations({ trips }) {
 
 			<h1 className="bold text-2xl text-white">List of Rotations</h1>
 
-			<table className="w-max table-auto border-collapse border-2 border-black text-center">
-				<thead>
-					<tr className="bg-gray-200">
-						<th>Trip Number</th>
-						<th>Rotation</th>
-						<th>Trip Length</th>
-						<th>Date</th>
-						<th>Start</th>
-						<th>End</th>
-						<th>Duration</th>
-						<th>Mileage</th>
-						<th>Layover</th>
-						<th>Letter</th>
-						<th>Number</th>
-						<th>Body</th>
-						<th>Flight Number</th>
-						<th>Credit Value</th>
-						<th>Time Away</th>
-					</tr>
-				</thead>
-				<tbody>
-					{trips.map((trip: Trip, index: number) => (
-						<Fragment key={trip.tripNumber}>
-							<tr
-								className={classNames(
-									'border-t-2 border-black',
-									index % 2 === 0 ? 'bg-gray-50' : 'bg-gray-200'
-								)}
-							>
-								<td rowSpan={trip.blocks.length}>{trip.tripNumber}</td>
-								<td rowSpan={trip.blocks.length}>{trip.rotation}</td>
-								<td rowSpan={trip.blocks.length}>{trip.tripLength}</td>
-
-								<BlockCols block={trip.blocks[0]} />
-
-								<td rowSpan={trip.blocks.length}>{trip.creditValue}</td>
-								<td rowSpan={trip.blocks.length}>{trip.timeAwayFromBase}</td>
-							</tr>
-
-							{trip.blocks.slice(1).map((block: Block, i: number) => (
+			<div
+				id="rotations-table"
+				className="h-full w-max max-w-full overflow-scroll rounded-md border-2 border-black"
+			>
+				<table className="relative -inset-[1px] table border-0 text-center">
+					<thead>
+						<tr className="bg-gray-200">
+							<th>Trip Number</th>
+							<th>Rotation</th>
+							<th>Trip Length</th>
+							<th>Date</th>
+							<th>Start</th>
+							<th>End</th>
+							<th>Duration</th>
+							<th>Mileage</th>
+							<th>Layover</th>
+							<th>Letter</th>
+							<th>Number</th>
+							<th>Body</th>
+							<th>Flight Number</th>
+							<th>Credit Value</th>
+							<th>Time Away</th>
+						</tr>
+					</thead>
+					<tbody>
+						{trips.map((trip: Trip, index: number) => (
+							<Fragment key={trip.tripNumber}>
 								<tr
-									key={`${trip.tripNumber}-${i}`}
-									className={index % 2 === 0 ? 'bg-gray-50' : 'bg-gray-200'}
+									className={classNames(
+										'border-t-2 border-black',
+										index % 2 === 0 ? 'bg-gray-50' : 'bg-gray-200'
+									)}
 								>
-									<BlockCols block={block} />
+									<td rowSpan={trip.blocks.length}>{trip.tripNumber}</td>
+									<td rowSpan={trip.blocks.length}>{trip.rotation}</td>
+									<td rowSpan={trip.blocks.length}>{trip.tripLength}</td>
+
+									<BlockCols block={trip.blocks[0]} />
+
+									<td rowSpan={trip.blocks.length}>{trip.creditValue}</td>
+									<td rowSpan={trip.blocks.length}>{trip.timeAwayFromBase}</td>
 								</tr>
-							))}
-						</Fragment>
-					))}
-				</tbody>
-			</table>
+
+								{trip.blocks.slice(1).map((block: Block, i: number) => (
+									<tr
+										key={`${trip.tripNumber}-${i}`}
+										className={index % 2 === 0 ? 'bg-gray-50' : 'bg-gray-200'}
+									>
+										<BlockCols block={block} />
+									</tr>
+								))}
+							</Fragment>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
