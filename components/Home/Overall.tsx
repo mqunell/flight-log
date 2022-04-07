2c0bc4d18162fb7f4883e911c66346be730b0d2b
Matt <mqunell@gmail.com>
2022-04-06 20:09:21 -0700

    finish refactoring Home as separate components

---
+++ components/Home/Overall.tsx
@@ -0,0 +1,52 @@
+import { Block, Trip } from '../../lib/trips';
+
+interface Props {
+	trips: Trip[];
+	blocks: Block[];
+}
+
+export default function Overall({ trips, blocks }: Props) {
+	// Calculate total values across all trips/blocks
+	const totals = {
+		trips: trips.length,
+		blocks: blocks.length,
+		daysFlown: new Set(blocks.map((block) => block.date)).size,
+		mileage: blocks.reduce((acc, block) => acc + block.mileage, 0),
+		duration: blocks.reduce((acc, block) => acc + block.duration, 0),
+		creditValue: trips.reduce((acc, trip) => acc + trip.creditValue, 0),
+		timeAwayFromBase: trips.reduce((acc, trip) => acc + trip.timeAwayFromBase, 0),
+		aircraft: {
+			airbus: blocks.filter((block) => block.aircraft.make === 'A').length,
+			boeing: blocks.filter((block) => block.aircraft.make === 'B').length,
+			other: blocks.filter((block) => block.aircraft.make === 'O').length,
+			narrow: blocks.filter((block) => block.aircraft.body === 'N').length,
+			wide: blocks.filter((block) => block.aircraft.body === 'W').length,
+		},
+	};
+
+	return (
+		<div className="grid grid-cols-2 gap-x-4 gap-y-1 border-t border-white px-4 py-2 text-white">
+			<OverallRow label="Trips" value={totals.trips} />
+			<OverallRow label="Blocks" value={totals.blocks} />
+			<OverallRow label="Days flown" value={totals.daysFlown} />
+			<OverallRow label="Miles" value={totals.mileage} />
+			<OverallRow label="Block time" value={totals.duration} />
+			<OverallRow label="Credit value" value={totals.creditValue} />
+			<OverallRow label="Time away" value={totals.timeAwayFromBase} />
+			<OverallRow label="Airbus" value={totals.aircraft.airbus} />
+			<OverallRow label="Boeing" value={totals.aircraft.boeing} />
+			<OverallRow label="Other" value={totals.aircraft.other} />
+			<OverallRow label="Narrowbody" value={totals.aircraft.narrow} />
+			<OverallRow label="Widebody" value={totals.aircraft.wide} />
+		</div>
+	);
+}
+
+function OverallRow({ label, value }) {
+	return (
+		<>
+			<p className="text-right">{label}</p>
+			<p className="font-bold">{value}</p>
+		</>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import Overall from '../components/Home/Overall';
 import UsaMap from '../components/Home/UsaMap';
 import { Trip, Block, getTrips } from '../lib/trips';
 
@@ -9,24 +10,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
 	const blocks: Block[] = [];
 	trips.forEach((trip: Trip) => blocks.push(...trip.blocks));
-
-	// Calculate total values across all trips/blocks
-	/* const totals = {
-		trips: trips.length,
-		blocks: allBlocks.length,
-		daysFlown: new Set(allBlocks.map((block) => block.date)).size,
-		mileage: allBlocks.reduce((acc, block) => acc + block.mileage, 0),
-		timeInAir: allBlocks.reduce((acc, block) => acc + block.duration, 0),
-		creditValue: trips.reduce((acc, trip) => acc + trip.creditValue, 0),
-		timeAwayFromBase: trips.reduce((acc, trip) => acc + trip.timeAwayFromBase, 0),
-		aircraft: {
-			airbus: allBlocks.filter((block) => block.aircraft.make === 'A').length,
-			boeing: allBlocks.filter((block) => block.aircraft.make === 'B').length,
-			other: allBlocks.filter((block) => block.aircraft.make === 'O').length,
-			narrow: allBlocks.filter((block) => block.aircraft.body === 'N').length,
-			wide: allBlocks.filter((block) => block.aircraft.body === 'W').length,
-		},
-	}; */
 
 	return { props: { trips, blocks } };
 };
@@ -47,31 +30,8 @@ export default function Home({ trips, blocks }) {
 				<a className="text-blue-300 hover:underline">Add</a>
 			</Link>
 
-			{/* <div className="grid grid-cols-2 gap-x-4 gap-y-1 border-t border-white px-4 py-2 text-white">
-				<TotalsRow label="Trips" value={totals.trips} />
-				<TotalsRow label="Blocks" value={totals.blocks} />
-				<TotalsRow label="Days flown" value={totals.daysFlown} />
-				<TotalsRow label="Miles" value={totals.mileage} />
-				<TotalsRow label="Block time" value={totals.timeInAir} />
-				<TotalsRow label="Credit value" value={totals.creditValue} />
-				<TotalsRow label="Time away" value={totals.timeAwayFromBase} />
-				<TotalsRow label="Airbus" value={totals.aircraft.airbus} />
-				<TotalsRow label="Boeing" value={totals.aircraft.boeing} />
-				<TotalsRow label="Other" value={totals.aircraft.other} />
-				<TotalsRow label="Narrowbody" value={totals.aircraft.narrow} />
-				<TotalsRow label="Widebody" value={totals.aircraft.wide} />
-			</div> */}
-
+			<Overall trips={trips} blocks={blocks} />
 			<UsaMap blocks={blocks} />
 		</div>
 	);
 }
-
-/* function TotalsRow({ label, value }) {
-	return (
-		<>
-			<p className="text-right">{label}</p>
-			<p className="font-bold">{value}</p>
-		</>
-	);
-} */
